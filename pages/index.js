8d9835096f6054cb20a87dcaf63ac9da3baf50fb
yibrino <yibrahmehari128@gmail.com>
2024-12-03 17:58:25 +0100

    dispatch all data done

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,12 @@ import ImageSlider from "../components/Home/Slider/ImageSlider";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllTeams } from "../features/team/helpers";
 import TeamsProfile from "../components/TeamsProfile/TeamsProfile";
+import { getAllCarousels } from "../features/carousel/helpers";
+import { getAllCategories } from "../features/categories/helpers";
+import { getAllEducations } from "../features/education/helpers";
+import { getAllProjects } from "../features/projects/helpers";
+import { getAllNews } from "../features/news/helpers";
+import { getAllSkills } from "../features/skill/helpers";
 export default function Home() {
   const dispatch = useDispatch();
   const [selectedTeam, setSelectedTeam] = useState(null); // Initialize as null to handle loading state
@@ -14,6 +20,12 @@ export default function Home() {
   useEffect(() => {
     // Dispatch to get all teams
     dispatch(getAllTeams());
+    dispatch(getAllCarousels());
+    dispatch(getAllCategories());
+    dispatch(getAllEducations());
+    dispatch(getAllProjects());
+    dispatch(getAllNews());
+    dispatch(getAllSkills());
 
     // Filter the first team where teamprofile_id equals 0
     const first_team = teams[0];
